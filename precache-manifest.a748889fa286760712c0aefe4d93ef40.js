12a4a076847ffbe3e855f81e6b084e1c99f5a46d
santius0 <santius1995@gmail.com>
2020-06-17 12:17:46 -0400

    Updates

---
+++ precache-manifest.a748889fa286760712c0aefe4d93ef40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743101063c7d98925faa0e87300d7097",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "b3e6bc9342a59079dedf",
+    "url": "/personal-site/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "292c0b03ddf1ecb1ac47",
+    "url": "/personal-site/static/css/main.699da05f.chunk.css"
+  },
+  {
+    "revision": "b3e6bc9342a59079dedf",
+    "url": "/personal-site/static/js/2.accc0232.chunk.js"
+  },
+  {
+    "revision": "aa515cbe958104d42ccceebac49f3f36",
+    "url": "/personal-site/static/js/2.accc0232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292c0b03ddf1ecb1ac47",
+    "url": "/personal-site/static/js/main.9e2f4f33.chunk.js"
+  },
+  {
+    "revision": "1f2cfef01d2bcee042e6",
+    "url": "/personal-site/static/js/runtime-main.afe5da4f.js"
+  },
+  {
+    "revision": "44663f8b1a7d734c4f33d83363151c03",
+    "url": "/personal-site/static/media/about.44663f8b.md"
+  },
+  {
+    "revision": "9c2fea805fd56c341d21d732dd647411",
+    "url": "/personal-site/static/media/me_icon.9c2fea80.jpg"
+  }
+]);
